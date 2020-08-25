4d7fb1316afa5ed6b1254f2ca4d43fef10e6e387
Madhon <madhon@madhon.com>
2020-08-25 09:53:15 +0100

    package updates

--- DesktopApplicationInsights.ExampleApp/Program.cs
+++ DesktopApplicationInsights.ExampleApp/Program.cs
@@ -2,9 +2,13 @@
 {
     using System;
     using System.Windows.Forms;
+    using Microsoft.ApplicationInsights;
 
     public static class Program
     {
+
+        public static TelemetryClient tc;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -14,7 +18,8 @@
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            var myClient = Telemetry.CreateClient("DesktopApplicationInsights.ExampleApp", "<instrumentation key from App Insights project>");
+            //tc = Telemetry.CreateClient("DesktopApplicationInsights.ExampleApp", "<instrumentation key from App Insights project>");
+            tc = Telemetry.CreateClient("DesktopApplicationInsights.ExampleApp", "21d580e9-681a-49d7-9f2c-9e85547a17b4");
 
             Application.Run(new Form1());
         }
